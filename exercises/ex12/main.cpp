7077dc7fc33a1fb1bef9042258c97c594130980f
Sindre Danielsen <122964950+zerath97@users.noreply.github.com>
2024-08-11 07:45:35 +0200

    Add files via upload

---
+++ exercises/ex12/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <chrono>
+#include <thread>
+#include <memory>
+
+#include <FL/Fl.H>
+#include <FL/Fl_Double_Window.H>
+
+#include "utilities.h"
+
+int main() {
+	auto win = std::make_unique<Fl_Double_Window>(screenWidth, screenHeight, "NASCAR");
+	win->color(FL_WHITE);
+
+	// Instansier nye objekt mellom dei to etterfølgjande linjene.
+	// -----------------------------------------------------------------------------
+
+	// -----------------------------------------------------------------------------
+
+	win->end();
+	win->show();
+
+	auto nextDrawing = std::chrono::steady_clock::now();
+
+	while (win->shown())
+	{
+		// Sov til neste teikning
+		std::this_thread::sleep_until(nextDrawing);
+		nextDrawing += std::chrono::microseconds(1'000'000 / 60);
+
+		Fl::check();	// Handterar input
+		Fl::redraw();	// Kallar draw
+	}
+}
