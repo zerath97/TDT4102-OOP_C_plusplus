86beed0cbeff29ad44ff4def0ee52947348c9559
Sindre Danielsen <122964950+zerath97@users.noreply.github.com>
2024-08-11 07:43:54 +0200

    Add files via upload

---
+++ exercises/ex10/MinesweeperWindow.cpp
@@ -0,0 +1,69 @@
+#include "MinesweeperWindow.h"
+
+MinesweeperWindow::MinesweeperWindow(Point xy, int width, int height, int mines, const string& title) :
+	Graph_lib::Window{xy, width * cellSize, height*cellSize, title}, width{width}, height{height}, mines{mines}
+	//Initialiser medlemsvariabler, bruker konstruktoren til Windowsklassen
+{
+	// Legg til alle tiles i vinduet
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			tiles.push_back(new Tile{ Point{j * cellSize, i * cellSize}, cellSize, cb_click });
+			attach(tiles.back());
+		}
+	}
+
+	//Legg til miner paa tilfeldige posisjoner
+
+
+	// Fjern window reskalering
+	resizable(nullptr);
+	size_range(x_max(), y_max(), x_max(), y_max());
+}
+
+
+vector<Point> MinesweeperWindow::adjacentPoints(Point xy) const {
+	vector<Point> points;
+	for (int di = -1; di <= 1; ++di) {
+		for (int dj = -1; dj <= 1; ++dj) {
+			if (di == 0 && dj == 0) {
+				continue;
+			}
+
+			Point neighbour{ xy.x + di * cellSize,xy.y + dj * cellSize };
+			if (inRange(neighbour)) {
+				points.push_back(neighbour);
+			}
+		}
+	}
+
+	return points;
+}
+
+void MinesweeperWindow::openTile(Point xy) {
+}
+
+void MinesweeperWindow::flagTile(Point xy) {
+}
+
+//Kaller openTile ved venstreklikk og flagTile ved hoyreklikk/trykke med to fingre paa mac
+void MinesweeperWindow::click()
+{
+	Point xy{Fl::event_x(), Fl::event_y()};
+
+	MouseButton mb = static_cast<MouseButton>(Fl::event_button());
+
+	if (!inRange(xy)) {
+		return;
+	}
+
+	switch (mb) {
+	case MouseButton::left:
+		openTile(xy);
+		break;
+	case MouseButton::right:
+		flagTile(xy);
+		break;
+	}
+
+	flush();
+}
---
+++ exercises/ex10/Tile.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "GUI.h"
+
+using namespace Graph_lib;
+
+//De forskjellige tilstandene en Tile kan vaere i
+enum class Cell { closed, open, flagged };
+
+class Tile : public Button
+{
+	Cell state = Cell::closed;
+
+	void set_label(string s) { label = s; redraw(); }
+	void set_label_color(Color c) { pw->labelcolor(c.as_int()); redraw();}
+public:
+	Tile(Point pos, int size, Graph_lib::Callback cb)
+		:Button{pos, size, size, "", cb} {};
+	
+	Cell getState() const {return state; };
+
+	void open();
+	void flag();
+};
