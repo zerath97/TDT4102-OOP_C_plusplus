46b2b203b4190cf2a4a52ed6dc668678b7b830f2
Sindre Danielsen <122964950+zerath97@users.noreply.github.com>
2024-08-11 07:30:40 +0200

    Create include.h

---
+++ exercises/ex2/include.h
@@ -0,0 +1,13 @@
+// lib
+#include "std_lib_facilities.h"
+#include <vector>
+#include "Graph.h"
+#include "Simple_window.h"
+
+// Project-Local functions
+#include "oppgaver/oppgave_1.h"
+#include "oppgaver/oppgave_2.h"
+#include "oppgaver/oppgave_4.h"
+#include "oppgaver/oppgave_5.h"
+#include "oppgaver/oppgave_6.h"
+#include "oppgaver/menu.h"
